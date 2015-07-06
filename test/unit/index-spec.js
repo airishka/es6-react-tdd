318bfcc8247fcf3bdcd1373ca025284d6f1a5332
airishka <ak.irishka@gmail.com>
2015-07-06 10:42:17 +0200

    Introduce .editorconfig

--- test/unit/index-spec.js
+++ test/unit/index-spec.js
@@ -1,6 +1,5 @@
 describe('initial test setup', function(){
-    it('should works', function(){
-        expect(1).toBe(1);
-    });
+  it('should works', function(){
+    expect(1).toBe(1);
+  });
 });
-
